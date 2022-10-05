d97c6f5ad09e8c6cab48299fceefa3ef76388e73
aybüke <aybukeegrbzz@gmail.com>
2022-10-05 23:37:58 +0300

    v1

---
+++ src/test/java/tests/day22/C01_RaporluTest.java
@@ -0,0 +1,53 @@
+package tests.day22;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BrcPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class C01_RaporluTest extends TestBaseRapor {
+
+    /*
+    -https://www.bluerentalcars.com/ adresine git
+    -login butonuna bas
+    -test data user email: customer@bluerentalcars.com ,
+    -test data password : 12345 dataları girip login e basın
+    -login butonuna tiklayin
+    -Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
+     */
+
+    @Test
+    public void test01() {
+
+        extentTest=extentReports.createTest("Pozitif Test","Geçerli kullanıcı adı ve password ile giriş yapıldı");
+        //-https://www.bluerentalcars.com/ adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        extentTest.info("Blue RentaCar sitesine gidildi");
+        //-login butonuna bas
+        BrcPage brc = new BrcPage();
+        brc.brclogin.click();
+        extentTest.info("Login butonuna basıldı");
+        //-test data user email: customer@bluerentalcars.com
+        //-test data password : 12345 dataları girip login e basın
+        //-login butonuna tiklayin
+        brc.UserEmail.sendKeys(ConfigReader.getProperty("userEmail"));
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("pass"))  // bir daha locate almamak için keys tab kullandık
+                .sendKeys(Keys.ENTER).perform();
+        extentTest.info("Doğru kullanıcı email ve password girildi");
+        extentTest.info("Ikinci login butonuna basıldı");
+
+        //-Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
+        String actualUserName = brc.basariliGiris.getText();
+        String expectedUserName = "John Walker";
+        Assert.assertEquals(expectedUserName, actualUserName);
+        extentTest.pass("Sayfaya basarili sekilde girildi");
+
+        //eğer hata raporu alırsak ve raporda resmi görüntülemek istersek rapor üzerine sağ click
+        //open, explorer secip resimli raporu görüntüleyebiliriz.
+
+}}
