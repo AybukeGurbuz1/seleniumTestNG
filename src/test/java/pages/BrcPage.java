2497e14206ae32e3f739e26254d8a16bf84ea942
aybüke <aybukeegrbzz@gmail.com>
2022-10-05 23:37:58 +0300

    v1

---
+++ src/test/java/pages/BrcPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class BrcPage {
+    public BrcPage() {  // constructor oluştur
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
+    public WebElement brclogin;
+
+    @FindBy(xpath = "//input[@id='formBasicEmail']")
+    public WebElement UserEmail;
+
+
+    @FindBy(xpath = "//*[@id='dropdown-basic-button']")
+    public WebElement basariliGiris;
+}
